7eebc74940ab925d4f7b3c44cbec3bf807603ba7
win2happy <102572353+win2happy@users.noreply.github.com>
2024-01-18 15:44:05 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Etornam VPN',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: customLightTheme(context),
       darkTheme: customDarkTheme(context),
       themeMode: ThemeMode.system,
